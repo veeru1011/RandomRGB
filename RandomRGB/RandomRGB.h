41d894329613d1ff9c39b959bbf47efe04509085
veer <veeru.ramjas@gmail.com>
2020-05-04 09:57:42 +0530

    Initial Commit

---
+++ RandomRGB/RandomRGB.h
@@ -0,0 +1,19 @@
+//
+//  RandomRGB.h
+//  RandomRGB
+//
+//  Created by mac on 04/05/20.
+//  Copyright © 2020 sample. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RandomRGB.
+FOUNDATION_EXPORT double RandomRGBVersionNumber;
+
+//! Project version string for RandomRGB.
+FOUNDATION_EXPORT const unsigned char RandomRGBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomRGB/PublicHeader.h>
+
+
